4859d0e7647ab3f06cf8314c4b27b8936f915408
AlmondMan <artusmosquet@gmail.com>
2023-12-08 13:45:04 -0500

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,50 +3,67 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-mode = input("Would you like to ENCODE or DECODE? ")
+invalid = True
 
-if mode == 'encode' or mode == 'ENCODE' or mode == 'e' or mode == "E" or mode == "Encode":
+while invalid:
 
-    msg = input("Your Message: ")
+    mode = input("Would you like to ENCODE or DECODE? ")
 
-    encoded = RCCG.encode(msg)
-
-    print(encoded)
-
-    file = input("Would you like to save to a file? (Y)yes/(N)no: ")
-    
-    if file == 'Y':
-        with open('RCCG-output.txt', 'w') as f:
-            f.write(encoded)
-            print("saved to 'output.txt'")
-    
-
-
-elif mode == 'decode' or mode == "DECODE" or mode == 'd' or mode == 'D' or mode == "Decode":
-
-    file = input("Would you like to decode from file or copy the encoded file? file/copy? ")
-
-    if file == "file":
+    if not mode in ['encode', 'ENCODE', 'e', 'E', 'Encode', 'decode', 'DECODE', 'd', 'D', 'Decode']:
         
-        root = tk.Tk()
-        root.withdraw()
-
-        filename = filedialog.askopenfilename(parent=root,title='Open file to encrypt')
-        contents = open(filename, 'r').read()
-
-        msg = contents
-        
+        print('Invalid input')
 
     else:
+        if mode in ['encode', 'ENCODE', 'e', 'E', 'Encode']:
 
-        msg = input("Coded Message: ")
-
-
-    decoded = RCCG.decode(msg)
-
-    print(decoded)
+            invalid = False
 
 
-input("press any key to finish: ")
+            msg = input("Your Message: ")
+
+            encoded = RCCG.encode(msg)
+
+            print(encoded)
+
+            file = input("Would you like to save to a file? y/n: ")
+            
+            if file == 'y':
+                with open('RCCG-output.txt', 'w') as f:
+                    f.write(encoded)
+                    print("saved to 'output.txt'")
+            
+
+
+        elif mode in ['decode', 'DECODE', 'd', 'D', 'Decode']:
+
+            invalid = False
+
+
+            file = input("Would you like to decode from file or copy the encoded file? file/copy? ")
+
+            if file == "file":
+                
+                root = tk.Tk()
+                root.withdraw()
+
+                filename = filedialog.askopenfilename(parent=root,title='Open file to encrypt')
+                contents = open(filename, 'r').read()
+
+                msg = contents
+                
+
+            else:
+
+                msg = input("Coded Message: ")
+
+
+            decoded = RCCG.decode(msg)
+
+            print(decoded)
+
+
+        input("press any key to finish: ")
+
+
 
         
